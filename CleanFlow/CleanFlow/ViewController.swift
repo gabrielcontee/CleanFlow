4f4dc5163901dc49dfffbefa2a653a373da7acd3
Gabriel Conte <gabrielconte14@gmail.com>
2020-04-15 14:54:56 -0300

    Xcode project created

---
+++ CleanFlow/CleanFlow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanFlow
+//
+//  Created by Gabriel Figueiredo Conde on 4/15/20.
+//  Copyright © 2020 Gabriel Figueiredo Conde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CleanFlow/CleanFlowTests/CleanFlowTests.swift
@@ -0,0 +1,34 @@
+//
+//  CleanFlowTests.swift
+//  CleanFlowTests
+//
+//  Created by Gabriel Figueiredo Conde on 4/15/20.
+//  Copyright © 2020 Gabriel Figueiredo Conde. All rights reserved.
+//
+
+import XCTest
+@testable import CleanFlow
+
+class CleanFlowTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
